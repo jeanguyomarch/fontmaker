8bb8a0899806367d310caf66144ca9a706fbc311
Jean Guyomarc'h <jean.guyomarch@gmail.com>
2015-05-10 11:11:28 +0200

    tests: display meaningfull info

--- tests/test.c
+++ tests/test.c
@@ -1,8 +1,24 @@
 #include "font.h"
+#include <stdio.h>
 
 int
 main(void)
 {
+   int w, h;
+
+   bounding_box_get(&w, &h);
+   printf("=== STATS ===\n"
+          "size.........: %i\n"
+          "w............: %i\n"
+          "h............: %i\n"
+          "glyphs.......: %i\n"
+          "color bits...: %u\n",
+          size_get(),
+          w,
+          h,
+          glyphs_available_get(),
+          color_bits_get());
+
    return 0;
 }
 
